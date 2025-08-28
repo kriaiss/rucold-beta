09187639efc6a38b64b07afcfd1a31f3313954e9
kriaiss <153354052+kriaiss@users.noreply.github.com>
2025-08-28 13:23:24 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,186 @@ let burger = true;
 
 $('.sphere').css('translate', '300');
 
+$('.image-button-1').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-1').css('display', 'flex')
+});
+
+$('#close-button-1').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-1').hide()
+});
+
+$('.image-button-2').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-2').css('display', 'flex')
+});
+
+$('#close-button-2').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-2').hide()
+});
+
+$('.image-button-3').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-3').css('display', 'flex')
+});
+
+$('#close-button-3').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-3').hide()
+});
+
+$('.image-button-4').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-4').css('display', 'flex')
+});
+
+$('#close-button-4').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-4').hide()
+});
+
+$('.image-button-5').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-5').css('display', 'flex')
+});
+
+$('#close-button-5').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-5').hide()
+});
+
+$('.image-button-6').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-6').css('display', 'flex')
+});
+
+$('#close-button-6').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-6').hide()
+});
+
+$('.image-button-7').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-7').css('display', 'flex')
+});
+
+$('#close-button-7').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-7').hide()
+});
+
+$('.image-button-8').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-8').css('display', 'flex')
+});
+
+$('#close-button-8').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-8').hide()
+});
+
+$('.image-button-9').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-9').css('display', 'flex')
+});
+
+$('#close-button-9').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-9').hide()
+});
+
+$('.image-button-10').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-10').css('display', 'flex')
+});
+
+$('#close-button-10').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-10').hide()
+});
+
+$('.image-button-11').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-11').css('display', 'flex')
+});
+
+$('#close-button-11').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-11').hide()
+});
+
+$('.image-button-12').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-12').css('display', 'flex')
+});
+
+$('#close-button-12').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-12').hide()
+});
+
+$('.image-button-13').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-2').css('display', 'flex')
+});
+
+$('#close-button-13').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-2').hide()
+});
+
+$('.image-button-14').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-14').css('display', 'flex')
+});
+
+$('#close-button-14').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-14').hide()
+});
+
+$('.image-button-15').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-15').css('display', 'flex')
+});
+
+$('#close-button-15').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-15').hide()
+});
+
+$('.image-button-16').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-16').css('display', 'flex')
+});
+
+$('#close-button-16').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-16').hide()
+});
+
+$('.image-button-17').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-17').css('display', 'flex')
+});
+
+$('#close-button-17').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-17').hide()
+});
+
+$('.image-button-18').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-18').css('display', 'flex')
+});
+
+$('#close-button-18').on('click', function() {
+    console.log('Клик зарегистрирован');
+    $('#fullscreen-photo-18').hide()
+});
+
 var animation_burger = anime({
     targets: '.burger-menu-button',
     duration: 500,
